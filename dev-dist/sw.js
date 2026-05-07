6d1bbc7fe286bf692a961ad949e5b0be4b55c71e
ShiponChowdhury1 <oxshipon1@gmail.com>
2026-05-07 10:18:31 +0600

    solved this error

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kflhp75du1s"
+    "revision": "0.qstihkfn5c4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
